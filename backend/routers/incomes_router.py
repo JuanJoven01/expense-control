7de55960926cbb01dd76e5799e7b6e26470fe3ad
JuanJoven01 <juanjov95@hotmail.com>
2024-02-07 18:36:20 -0500

    services to incomes finished

--- backend/routers/incomes_router.py
+++ backend/routers/incomes_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from middlewares.auth import JWTBearer
 
-from services.incomes_services import get_own_incomes, new_own_income, update_own_income
+from services.incomes_services import get_own_incomes, new_own_income, update_own_income, delete_own_income, get_team_incomes,new_team_incomes, update_team_incomes, delete_team_incomes
 
 from schemas.incomes_schemas import Incomes, IncomesUpdate
 
@@ -45,3 +45,44 @@ def update_income(income: IncomesUpdate, jwt_payload =  Depends(JWTBearer())):
         return update_own_income(user_id, income_name,income_description,income_date, amount,wallet_id, category_id, income_id)
     except Exception as e:
         return {'service error': str(e)}
+    
+@incomes_router.delete('/incomes/delete/{id}', tags=['incomes'],dependencies=[Depends(JWTBearer())])
+def delete_income(id: int, jwt_payload = Depends(JWTBearer())):
+    try:
+        return delete_own_income(id, jwt_payload['user_id'])
+    except Exception as e:
+        return {'router error': str(e)}
+    
+@incomes_router.get('/incomes/teams/get/{team_id}', tags=['incomes'], dependencies=[Depends(JWTBearer())])
+def get_teams_incomes(team_id:int, jwt_payload = Depends(JWTBearer())):
+    try:
+        username = jwt_payload['user']
+        user_id = jwt_payload['user_id']
+        return get_team_incomes(username, user_id, team_id)
+    except Exception as e:
+        return {'router error': str(e)}
+    
+@incomes_router.post('/incomes/teams/new/{team_id}', tags=['incomes'], dependencies=[Depends(JWTBearer())])
+def new_teams_incomes(team_id, income:Incomes, jwt_payload=Depends(JWTBearer())):
+    try:
+        username = jwt_payload['user']
+        return new_team_incomes(username, team_id, income)
+    except Exception as e:
+        return {'router error': str(e)}
+    
+@incomes_router.patch('/incomes/teams/patch/{team_id}', tags=['incomes'], dependencies=[Depends(JWTBearer())])
+def update_teams_incomes(team_id, income: IncomesUpdate, jwt_payload= Depends(JWTBearer())):
+    try:
+       username = jwt_payload['user']
+       return  update_team_incomes( username, team_id, income)
+
+    except Exception as e:
+        return {'router error': str(e)}
+    
+@incomes_router.delete('/incomes/teams/remove/teams/{team_id}/{income_id}', tags=['incomes'], dependencies=[Depends(JWTBearer())])
+def delete_teams_incomes(team_id:int, income_id:int, jwt_payload = Depends(JWTBearer())):
+    try:
+        username = jwt_payload['user']
+        return delete_team_incomes(username, team_id, income_id)
+    except Exception as e:
+        return {'router error': str(e)}
--- backend/services/incomes_services.py
+++ backend/services/incomes_services.py
@@ -8,6 +8,8 @@ from db.models.models import Incomes
 
 from services.sql_services import to_dict
 
+from services.teams_services import __verify_if_user_in_teams__
+
 def get_own_incomes(user_id:int):
     try:
         with Session() as session:
@@ -43,8 +45,6 @@ def update_own_income(user_id:int, name:str, description:str, datetime: datetime
                         .where(Incomes.id == income_id)
                     )
                     income_to_update = session.execute(update_query).scalar()
-                    print('='*30)
-                    print(income_to_update)
                     income_to_update.name = name
                     income_to_update.description = description
                     income_to_update.datetime = datetime
@@ -54,5 +54,88 @@ def update_own_income(user_id:int, name:str, description:str, datetime: datetime
                     session.commit()
                 return {'message':'income updated'}
         return {'error': "looks like it's not your income"}
+    except Exception as e:
+        return {'service error': str(e)}
+    
+def delete_own_income(income_id:int, user_id:int):
+    try:
+        raw_incomes =  get_own_incomes(user_id)
+        incomes = [to_dict(income) for income in raw_incomes]
+        for income in incomes:
+            if income['id'] == income_id:
+                with Session() as session:
+                    income = session.get(Incomes, income_id)
+                    session.delete(income)
+                    session.commit()
+                    return {'message':'income removed'}
+        return {'error': "looks like it's not your income"}
+                    
+    except Exception as e:
+        return {'service error': str(e)}
+    
+def get_team_incomes(username:str, team_id: int):
+    try:
+        if __verify_if_user_in_teams__(username, team_id):
+            with Session() as session:
+                query = (
+                    select(Incomes)
+                    .where(Incomes.team_id==team_id)
+                )
+                incomes = session.execute(query).scalars().all()
+                incomes_list = [to_dict(income) for income in incomes]
+                return incomes_list
+        return {'error': 'looks like you are not bellow to the team'}
+    except Exception as e:
+        return {'service error': str(e)}
+    
+
+def new_team_incomes(username:str, team_id:int, income:dict):
+    try:
+        if __verify_if_user_in_teams__(username, team_id):
+            with Session() as session:
+                new_income = Incomes(name=income.name, description=income.description, amount=income.amount, datetime=income.date, team_id=team_id, wallet_id=income.wallet_id, category_id=income.category_id)
+                session.add(new_income)
+                session.commit()
+                return {'message':'income added'}
+        return {'error': 'looks like you are not bellow to the team'}    
+
+    except Exception as e:
+        return {'service error': str(e)}
+    
+def update_team_incomes(username: str, team_id: int, income:dict):
+    try:
+        current_incomes = get_team_incomes(username, team_id)
+        for current_income in current_incomes:
+            if current_income['id'] == income.income_id:
+                with Session() as session:
+                    update_query = (
+                        select(Incomes)
+                        .where(Incomes.id == income.income_id)
+                    )
+                    income_to_update = session.execute(update_query).scalar()
+                    income_to_update.name = income.name
+                    income_to_update.description = income.description
+                    income_to_update.amount = income.amount
+                    income_to_update.datetime = income.date
+                    income_to_update.wallet_id = income.wallet_id
+                    income_to_update.category_id = income.category_id
+                    session.commit()
+                    return {'message': 'income updated'}
+        return {'error': "looks like it's not your income"} 
+
+    except Exception as e:
+        return {'service error': str(e)}
+    
+def delete_team_incomes(username: str, team_id:int, income_id:int):
+    try:
+        incomes = get_team_incomes(username, team_id)
+        for income in incomes:
+            if income['id'] == income_id:
+                with Session() as session:
+                    income_to_delete = session.get(Incomes, income_id)
+                    session.delete(income_to_delete)
+                    session.commit()
+                    return {'message':'income removed'}
+        return {'error': "looks like it's not your income"} 
     except Exception as e:
         return {'service error': str(e)}
